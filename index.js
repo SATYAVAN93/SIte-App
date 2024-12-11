f4849f507c54cb0af90c0d48354aaedd3364e6bf
Satyavan <166908829+SATYAVAN93@users.noreply.github.com>
2024-12-11 09:50:15 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+<><script async src="https://www.googletagmanager.com/gtag/js?id=G-M8S4MT3EYG"></script><script>
+   window.dataLayer = window.dataLayer || [];
+   function gtag() {dataLayer.push(arguments)};
+   &rbrace;
+   gtag("js", new Date());
+
+   gtag("config", "G-M8S4MT3EYG");
+</script><script type="text/javascript">
+      (function (c, l, a, r, i, t, y) {c[a] =
+         c[a] ||
+         function () {
+            (c[a].q = c[a].q || []).push(arguments);
+         } };
+      t = l.createElement(r);
+      t.async = 1;
+      t.src = "https://www.clarity.ms/tag/" + i;
+      y = l.getElementsByTagName(r)[0];
+      y.parentNode.insertBefore(t, y);
+      &rbrace;)(window, document, "clarity", "script", "kss8za9j58");
+   </script></>
